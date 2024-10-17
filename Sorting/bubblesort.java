90f832cadbdef999584dcc8357cd12e7d32c3ed6
Akshat <akshattated99@gmail.com>
2024-10-17 19:49:09 +0530

    Bubble Sort Added

--- Sorting/bubblesort.java
+++ Sorting/bubblesort.java
@@ -14,12 +14,17 @@ public class bubblesort {
         }
               
         for (int i=0;i<n-1;i++){
+            boolean swapped=false;
             for (int j=0;j<n-1-i;j++){
                 if(arr[j]>arr[j+1]){
                     temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                 }
+            }
+            if(!swapped){
+                break;
             }
         }
         System.out.println("Array after sorting in Ascending order: " + Arrays.toString(arr));
